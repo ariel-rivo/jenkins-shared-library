0ad7f27a57c47282f9783b5adcf77b08b44d857c
ariel-rivo <191101378+ariel-rivo@users.noreply.github.com>
2025-02-14 18:34:39 +0000

    groovy the worst

--- src/libs/awsLib.groovy
+++ src/libs/awsLib.groovy
@@ -1,31 +1,15 @@
 package libs
-@GrabResolver(name='central', root='https://repo1.maven.org/maven2/')
-@Grab(group='software.amazon.awssdk', module='sts', version='2.20.0')
-@Grab(group='software.amazon.awssdk', module='regions', version='2.20.0')
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sts.StsClient;
-import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
-import software.amazon.awssdk.services.sts.model.StsException;
-import software.amazon.awssdk.services.sts.model.AssumeRoleResponse;
-import software.amazon.awssdk.services.sts.model.Credentials;
+@Grab('com.amazonaws:aws-java-sdk:1.11.89')
 
-Region region = Region.US_EAST_1;
-StsClient stsClient = StsClient.builder()
-    .region(region)
-    .build();
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
 
-AssumeRoleRequest assumeRoleRequest = AssumeRoleRequest.builder()
-    .roleArn("arn:aws:iam::123456789012:role/demo")
-    .roleSessionName("session1")
-    .build();
+    def regionList() {
+        ec2 = AmazonEC2ClientBuilder.defaultClient()
+        regions = ec2.describeRegions().getRegions()
 
-try {
-    AssumeRoleResponse assumeRoleResponse = stsClient.assumeRole(assumeRoleRequest);
-    Credentials credentials = assumeRoleResponse.credentials();
-    System.out.println("Access Key ID: " + credentials.accessKeyId());
-    System.out.println("Secret Access Key: " + credentials.secretAccessKey());
-    System.out.println("Session Token: " + credentials.sessionToken());
-} catch (StsException e) {
-    System.err.println(e.getMessage());
-    System.exit(1);
-}
+        regionlist = ""
+        regions.each {
+            regionlist += it.getRegionName() + "\n"
+        }
+        return regionlist
+    }
